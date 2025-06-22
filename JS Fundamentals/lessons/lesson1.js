91a58d0a7c8e8a79031bea7ac4fe3a93c1cb0b2b
Hunter Snyder <hsnyder315@gmail.com>
2025-06-22 13:16:03 -0500

    Lesson1

---
+++ JS Fundamentals/lessons/lesson1.js
@@ -0,0 +1,20 @@
+// 1. Hello World
+console.log("Hello World!")
+
+// 2. Variables, Constants, and Data Types
+// Variables
+// Defined with keywords var and let
+
+var firstName = "Hunter"
+let lastName = "Snyder"
+console.log(firstName + " " + lastName)
+
+var age, dateOfBirth, sex
+age = "33"
+sex = "Male"
+console.log(age)
+age = "34"
+console.log(age)
+
+// Constants
+// Defined with keyword const
