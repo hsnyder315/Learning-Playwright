0545c36d893825f0dda5fe78be1a154d88e37325
Hunter Snyder <hsnyder315@gmail.com>
2025-06-22 16:02:04 -0500

    Create lesson4.js

---
+++ JS-Fundamentals/lessons/lesson4.js
@@ -0,0 +1,16 @@
+// Relational and Equality Operators
+// Relational
+// > - more than
+// < -  less than
+// >= - more than or equal
+// <= - less than or equal
+
+var result = 6 >= 5
+console.log(result)
+
+// Equality
+
+var x = 1
+console.log(x == '1') // True, lose comparison
+console.log(x === '1') // False, strict comparison. x = a number value, not string
+console.log(x === 1) // True
