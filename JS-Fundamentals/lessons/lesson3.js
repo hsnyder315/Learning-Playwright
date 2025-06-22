61792d8c63d3add71e487c6349972823437d317a
Hunter Snyder <hsnyder315@gmail.com>
2025-06-22 15:48:28 -0500

    Update lesson3.js

--- JS-Fundamentals/lessons/lesson3.js
+++ JS-Fundamentals/lessons/lesson3.js
@@ -1,9 +1,21 @@
 // Objects & Arrays
-//Objects
+// Objects
+// Defined by {}
 
 var customer = {
     firstName: 'Hunter',
     lastName: 'Snyder',
+    cars: ["Volvo", "Volkswagon", "Germany"]
 }
+// Dot notation
+customer.firstName = 'John'
+// Bracket notation
+customer['lastName'] = 'Silver'
+console.log(`${customer.firstName} ${customer.lastName}`)
 
-console.log(customer['lastName'])
+// Arrays
+// Defined by []
+var car = ["Volvo", "Volkswagon", "Germany"]
+car[1] = "BMW"
+console.log(car[1])
+console.log(customer.cars[0])
